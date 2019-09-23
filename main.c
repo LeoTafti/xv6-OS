6c8a9033e7235f65180abc540029ffdaa52a04a6
Leo Tafti <leotafti@gmail.com>
2019-09-23 08:13:26 -0400

    Made ppages_info smaller (probably still too big) and tried something new with freelist declaration

--- main.c
+++ main.c
@@ -13,7 +13,8 @@ static void startothers(void);
 static void mpmain(void)  __attribute__((noreturn));
 extern pde_t *kpgdir;
 extern char end[]; // first address after kernel loaded from ELF file
-extern struct page_info *ppages_info;
+//extern struct page_info *ppages_info; TODO : remove
+extern struct page_info *freelist
 
 #if LAB >= 2    // ...then leave this code out.
 #elif LAB >= 1
@@ -34,7 +35,7 @@ int
 test_page_free_list()
 {
 	//Check the page free list is not corrupted
-  cprintf("Test accessing ppages_info from main.c test_page_free_list() : %p", ppages_info);
+  cprintf("Test accessing freelist from main.c test_page_free_list() : %p", freelist);
 
 	//Check that the pages that should not be free are not on the list of free pages
 
