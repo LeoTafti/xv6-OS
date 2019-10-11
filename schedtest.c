179d88cd823cbbd2179d928c290f8b52b160ad6c
Leo Tafti <ltafti3@gatech.edu>
2019-10-11 08:12:37 -0400

    Corrected schedtest.c

--- schedtest.c
+++ schedtest.c
@@ -2,24 +2,32 @@
 #include "user.h"
 
 
-#define NB_CHILDREN 2
-#define OCCUPY_LEN 10000
+#define NB_FORKS 4
+#define OCCUPY_LEN 20000000
+
 #define NB_CHUNKS 10
 #define CHUNK_LEN (OCCUPY_LEN / NB_CHUNKS)
 
-void occupy(int pid){
+void occupy(int id){
     for(int i = 0; i < OCCUPY_LEN; i++){
         if(i % CHUNK_LEN == 0){
-            printf(1, "pid : %d – done %d / %d", pid, i/CHUNK_LEN, NB_CHUNKS);
+            printf(1, "process : %d – done %d / %d\n", id, i/CHUNK_LEN + 1, NB_CHUNKS);
         }
     }
+    printf(1, "–––\n");
 }
 
 int main(void){
-    int pid;
-    for(int i = 0; i < NB_CHILDREN; i++){
-        if((pid = fork()) != -1){
-            occupy(pid);
+    int pid, i;
+    for(i = 0; i < NB_FORKS; i++){
+        if(fork() == 0){ //fork() returns 0 in child proc
+            occupy(i);
+            exit();
         }
     }
+
+    for(i = 0; i < NB_FORKS; i++) //Wait for each child
+        wait();
+
+    exit();
 }
