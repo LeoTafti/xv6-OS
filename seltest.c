9ddafa7ed59f7416d6f5b57e4db074a9ffa7c954
Leo Tafti <leotafti@gmail.com>
2019-11-10 08:26:57 -0500

    waiting simple test

--- seltest.c
+++ seltest.c
@@ -19,22 +19,22 @@ int get_nfds(fd_set readset, fd_set writeset){
 /**
  * @brief Helper function to print the result of a test to the console
  */
-void print_result(int test_num, int select_retval, int success_condition){
+void print_result(char *test_name, int select_retval, int success_condition){
   if(select_retval < 0){
-      printf(2, "Test %d : ERROR\n", test_num);
+      printf(2, "Test %s : ERROR\n", test_name);
       return;
   }
   
   if(success_condition)
-      printf(1, "Test %d : SUCCESS\n", test_num);
+      printf(1, "Test %s : SUCCESS\n", test_name);
   else
-      printf(1, "Test %d : FAIL\n", test_num);
+      printf(1, "Test %s : FAIL\n", test_name);
 }
 
 /**
  * @brief Ensure that pipes and console properly detect when they are readable/writable
  */
-void basic(){
+void basic_test(){
   int r;
 
   int p[2];
@@ -49,13 +49,13 @@ void basic(){
   // Test 1 : Console should always be writable
   FD_SET(0, &writefds);
   r = select(1, &readfds, &writefds);
-  print_result(1, r, r == 1 && FD_ISSET(0, writefds));
+  print_result("basic 1", r, r == 1 && FD_ISSET(0, writefds));
 
   // Test 2 : Console shouldn't be readable before we write anything to it //TODO : is this correct ?
   // Note : we let fd0 still set in writefds s.t. select doesn't wait
   FD_SET(0, &readfds);
   r = select(1, &readfds, &writefds);
-  print_result(2, r, r == 1 && FD_ISSET(0, writefds) && !FD_ISSET(0, readfds));
+  print_result("basic 2", r, r == 1 && FD_ISSET(0, writefds) && !FD_ISSET(0, readfds));
 
   // Test 3 : Pipe shouldn't be readable when empty, but should be writable
   pipe(p);
@@ -69,7 +69,7 @@ void basic(){
   printf(1, "Check that get_nfds works properly : *p_out = %d, *p_in = %d, get_nfds(…) = %d\n", *p_out, *p_in, get_nfds(readfds, writefds));
 
   r = select(get_nfds(readfds, writefds), &readfds, &writefds);
-  print_result(3, r, r == 1 && FD_ISSET(*p_in, writefds) && !FD_ISSET(*p_out, &readfds));
+  print_result("basic 3", r, r == 1 && FD_ISSET(*p_in, writefds) && !FD_ISSET(*p_out, &readfds));
 
   // Test 4 : Pipe should become readable once some data has been written to it
   char txt[] = "Hello world";
@@ -81,7 +81,7 @@ void basic(){
   FD_SET(*p_in, &writefds); //To check select's behavior with both readable and writable fd's
 
   r = select(get_nfds(readfds, writefds), &readfds, &writefds);
-  print_result(4, r, r == 2 && FD_ISSET(*p_in, writefds) && FD_ISSET(*p_out, &readfds));
+  print_result("basic 4", r, r == 2 && FD_ISSET(*p_in, writefds) && FD_ISSET(*p_out, &readfds));
 
   // Test 5 : Pipe should not be readable after it gets emptied again.
   char buf[5];
@@ -89,7 +89,7 @@ void basic(){
 
   //Note : we let *p_in fd set in writefds, s.t. select doesn't wait.
   r = select(get_nfds(readfds, writefds), &readfds, &writefds);
-  print_result(5, r, r == 1 && FD_ISSET(*p_in, writefds) && !FD_ISSET(*p_out, &readfds));
+  print_result("basic 5", r, r == 1 && FD_ISSET(*p_in, writefds) && !FD_ISSET(*p_out, &readfds));
 
   // Test 6 : Pipe should become readable again if we close its write fd(s)
   close(*p_in);
@@ -98,10 +98,58 @@ void basic(){
   //*p_out still set in readfds
 
   r = select(get_nfds(readfds, writefds), &readfds, &writefds);
-  print_result(6, r, r == 1 && FD_ISSET(*p_out, &readfds));
+  print_result("basic 6", r, r == 1 && FD_ISSET(*p_out, &readfds));
+
+  close(*p_in);
+  close(*p_out);
+}
+
+/**
+ * @brief Ensure that select properly waits and wakes up
+ */
+void waiting_test(){
+  int r, nfds;
+
+  fd_set readfds, writefds;
+  FD_ZERO(&readfds);
+  FD_ZERO(&writefds);
+
+  int p[2];
+  int *p_out = &p[0];
+  int *p_in = &p[1];
+  pipe(p);
+
+  FD_SET(*p_out, &readfds);
+
+  //Fork a child. Child sleeps for some time, then puts data into the pipe and sleeps again.
+  //Parent calls select right away, trying to read from the end of the pipe (should result in parent waiting);
+  if(fork() == 0){
+    printf(1, "Child : created, about to sleep.");
+    sleep(400);
+    printf(1, "Child : writing to pipe\n");
+    write(*p_in, "Hello", 6);
+    printf(1, "Child : sleeping again\n");
+    sleep(400);
+    printf(1, "Child : exiting\n");
+    close(*p_in);
+    close(*p_out);
+    exit();
+  }
+
+  printf(1, "Parent : calling select\n");
+  r = select(*p_out + 1, &readfds, &writefds);
+  printf(1, "Parent : back from select\n");
+
+  //TODO : need to wait() somewhere ?
+
+  print_result("waiting 1", r, r == 1 && FD_ISSET(*p_out, &readfds));
+
+  close(*p_in);
+  close(*p_out);
 }
 
 int main(void){
-    basic();
+    //basic_test();
+    waiting_test();
     exit();
 }
