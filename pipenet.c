d303baff0e73d699731d16c31d7c6013ec560a56
Leo Tafti <leotafti@gmail.com>
2019-11-14 12:18:02 -0500

    Multiplexing pipenet.c

--- pipenet.c
+++ pipenet.c
@@ -4,13 +4,14 @@
 #include "syscall.h"
 #include "select.h"
 
+#define BUFSIZE 100
 
 char * shargv[] = { "sh", 0 };
 
 void pipenet(void) {
   int toshfds[2], fromshfds[2];
-  char buf[100];
-  int bytesread;
+  char buf1[BUFSIZE];
+  char buf2[BUFSIZE];
   int pid;
 
   //fd_set readfds, writefds;
@@ -42,30 +43,79 @@ void pipenet(void) {
     close(toshfds[0]);  // close read end
     close(fromshfds[1]);// close write end
 
+    fd_set readfds, writefds;
+    int bytesbuffered1 = 0, bytesbuffered2 = 0;
+    
+    //Compute nfds value
+    int maxfd = fromshfds[0] > toshfds[1] ? fromshfds[0] : toshfds[1];
+    int nfds = maxfd + 1;
+
     printf(1,"\n\n ***** Starting PipeNet ***** \n\n");
     for (;;) {
-      //printf(1, "\nReading from console\n");
-      bytesread = read(0, buf, sizeof(buf)); // read from console
-      //printf(1, "\nWriting to pipe\n");
-      write(toshfds[1], buf, bytesread); // write console data to pipe
-      for (;;) {    
-        //printf(1, "\nReading from pipe\n");
-        bytesread = read(fromshfds[0], buf, sizeof(buf)); // read from pipe
-        if (bytesread > 0) {
-          //printf(1, "\nWriting to console\n");
-          // write data from pipe to console
-          if (write(1, buf, bytesread) < 0) {
-            printf(2, "Write error!");
-            exit();
-          }
-          // no data left to read
-        } else if (bytesread == 0) {
-          break;
-          // error
-        } else {
-          printf(2, "Pipe read error!");
+
+      //Initialize
+      FD_ZERO(&readfds);
+      FD_ZERO(&writefds);
+
+      //Set readfds
+      FD_SET(0, &readfds);
+      FD_SET(fromshfds[0], &readfds);
+      
+      //Set writefds
+      FD_SET(toshfds[1], &writefds);
+      FD_SET(1, &writefds);
+
+      if(select(nfds, &readfds, &writefds) < 0){
+        printf(2, "Select failed\n");
+        exit();
+      }
+
+
+      if(FD_ISSET(0, &readfds) && bytesbuffered1 < BUFSIZE){
+        //printf(1, "\nReading from console\n");
+        int bytesread = read(0, buf1 + bytesbuffered1, sizeof(buf1) - bytesbuffered1); // read from console
+        if(bytesread < 0){
+          printf(2, "Read error (1)");
           exit();
         }
+        bytesbuffered1 += bytesread;
+      }
+
+      if(FD_ISSET(toshfds[1], &writefds) && bytesbuffered1 > 0){ //bytesbuffered > 0 checks that we have something to write in the buffer
+        //printf(1, "\nWriting to pipe\n");
+        int byteswritten = write(toshfds[1], buf1, bytesbuffered1); // write console data to pipe
+        if(byteswritten < 0){
+          printf(2, "Write error (1)");
+          exit();
+        }
+
+        //Move remaining bytes (if any) to the beggining of the buffer (no circular buffer for simplicity)
+        memmove(buf1, buf1+byteswritten, bytesbuffered1 - byteswritten);
+
+        //Update bytesbuffered
+        bytesbuffered1 -= byteswritten;
+      }
+
+      if(FD_ISSET(fromshfds[0], &readfds) && bytesbuffered2 < BUFSIZE){
+          //printf(1, "\nReading from pipe\n");
+          int bytesread = read(fromshfds[0], buf2 + bytesbuffered2, sizeof(buf2) - bytesbuffered2); // read from pipe
+          if(bytesread < 0){
+            printf(2, "Read error (2)");
+            exit();
+          }
+          bytesbuffered2 += bytesread;
+      }
+
+      if(FD_ISSET(1, &writefds) && bytesbuffered2 > 0){
+        int byteswritten = write(1, buf2, bytesbuffered2);
+        if(byteswritten < 0){
+          printf(2, "Write error (2)");
+          exit();
+        }
+
+        memmove(buf2, buf2+byteswritten, bytesbuffered2 - byteswritten);
+
+        bytesbuffered2 -= byteswritten;
       }
     }
     wait();
